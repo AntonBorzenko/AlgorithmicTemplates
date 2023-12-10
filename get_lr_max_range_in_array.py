7a19d6a47ca2ac9cbfcb653ee640e49debdc7814
Anton Borzenko <borzenko_a@bk.ru>
2023-12-10 09:51:09 +0100

    Add get_min_range

--- get_lr_max_range_in_array.py
+++ get_lr_max_range_in_array.py
@@ -10,18 +10,21 @@ left, right = get_max_range(array)
 from typing import Callable
 
 
-def get_max_range(array: list, key: Callable[[int], int] = None):
+def get_max_range(array: list, key: Callable[[int], int] = None, borders=(False, True)):
     if key is None:
         def key(value):
             return value
 
     length = len(array)
+    left_border, right_border = borders
 
     stack = []
     right = [0] * length
     for i in range(length):
         current_key = key(array[i])
-        while stack and current_key > stack[-1][1]:
+        while stack and (
+            current_key > stack[-1][1] or (not right_border and current_key == stack[-1][1])
+        ):
             right[stack.pop()[0]] = i - 1
         stack.append((i, current_key))
     for i, _ in stack:
@@ -31,10 +34,20 @@ def get_max_range(array: list, key: Callable[[int], int] = None):
     left = [0] * length
     for i in range(length - 1, -1, -1):
         current_key = key(array[i])
-        while stack and current_key >= stack[-1][1]:
+        while stack and (
+            current_key > stack[-1][1] or (not left_border and current_key == stack[-1][1])
+        ):
             left[stack.pop()[0]] = i + 1
         stack.append((i, current_key))
     for i, _ in stack:
         left[i] = 0
 
     return left, right
+
+
+def get_min_range(array: list, key: Callable[[int], int] = None, borders=(False, True)):
+    if key is None:
+        def key(value):
+            return -value
+
+    return get_max_range(array, key, borders)
---
+++ tests/test__get_lr_max_range_in_array.py
@@ -0,0 +1,58 @@
+from tasks_templates.get_lr_max_range_in_array import get_max_range, get_min_range
+
+
+def test__default_settings():
+    assert get_max_range([1, 2, 5, 5, 7, 3, 10]) == (
+        [0, 0, 0, 3, 0, 5, 0],
+        [0, 1, 3, 3, 5, 5, 6]
+    )
+
+
+def test__border_values():
+    assert get_max_range([5, 5, 5, 7, 7, 10, 11, 11]) == (
+        [0, 1, 2, 0, 4, 0, 0, 7],
+        [2, 2, 2, 4, 4, 5, 7, 7]
+    )
+
+
+def test__border_values_2():
+    assert get_max_range([8, 8, 8, 7, 7, 12, 11, 11]) == (
+        [0, 1, 2, 3, 4, 0, 6, 7],
+        [4, 4, 4, 4, 4, 7, 7, 7]
+    )
+
+
+def test__flag_ff():
+    assert get_max_range([8, 8, 8, 7, 7, 12, 12, 11, 11], borders=(False, False)) == (
+        [0, 1, 2, 3, 4, 0, 6, 7, 8],
+        [0, 1, 4, 3, 4, 5, 8, 7, 8]
+    )
+
+
+def test__flag_tf():
+    assert get_max_range([8, 8, 8, 7, 7, 12, 12, 11, 11], borders=(True, False)) == (
+        [0, 0, 0, 3, 3, 0, 0, 7, 7],
+        [0, 1, 4, 3, 4, 5, 8, 7, 8]
+    )
+
+
+def test__flag_tt():
+    assert get_max_range([8, 8, 8, 7, 7, 12, 12, 11, 11], borders=(True, True)) == (
+        [0, 0, 0, 3, 3, 0, 0, 7, 7],
+        [4, 4, 4, 4, 4, 8, 8, 8, 8]
+    )
+
+
+def test__get_min_range():
+    assert get_min_range([8, 8, 8, 7, 7, 12, 12, 11, 11]) == (
+        [0, 1, 2, 0, 4, 5, 6, 5, 8],
+        [2, 2, 2, 8, 8, 6, 6, 8, 8]
+    )
+
+
+def test__get_min_range_tt():
+    assert get_min_range([8, 8, 8, 7, 7, 12, 12, 11, 11], borders=(True, True)) == (
+        [0, 0, 0, 0, 0, 5, 5, 5, 5],
+        [2, 2, 2, 8, 8, 6, 6, 8, 8]
+    )
+
