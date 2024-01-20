0c1d89bdcea50eaff71716bb73ebd8f494c662f8
Anton Borzenko <borzenko_a@bk.ru>
2024-01-20 18:02:41 +0100

    Move binary_search comments

--- binary_search/binary_search.py
+++ binary_search/binary_search.py
@@ -1,4 +1,15 @@
-def bisearch(check, lo, hi, false2true=True, right_border=True):
+"""
+How to use:
+
+bisearch(lambda v: v >= 15, lo=0, hi=100, right_border=True) == 15
+bisearch(lambda v: v >= 15, lo=0, hi=100, right_border=False) == 14
+
+bisearch(lambda v: v <= 14, lo=0, hi=100, false2true=False, right_border=True) == 15
+bisearch(lambda v: v <= 14, lo=0, hi=100, false2true=False, right_border=False) == 14
+"""
+
+
+def bisearch(check, lo: int, hi: int, false2true=True, right_border=True):
     not_false2true = not false2true
     not_right_border = not right_border
     while lo < hi:
