b8155f80ecb776bf2d95e790de10d1231367c206
Anton Borzenko <borzenko_a@bk.ru>
2024-03-01 00:05:11 +0100

    Add string function

---
+++ strings/zfunction.py
@@ -0,0 +1,15 @@
+def z_function(string: str) -> list[int]:
+    n = len(string)
+    result = [0] * n
+
+    left = 0
+    right = 0
+    for i in range(1, n):
+        result[i] = max(0, min(right - i, result[i - left]))
+        while i + result[i] < n and string[result[i]] == string[i + result[i]]:
+            result[i] += 1
+        if i + result[i] > right:
+            left = i
+            right = i + result[i]
+
+    return result
